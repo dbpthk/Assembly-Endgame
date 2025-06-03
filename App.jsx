d139cd8ea482acfd5819a8e5ed5fb9001d7e572c
Dhruv <dbpthk@gmail.com>
2025-06-03 20:14:45 +1000

    Main page JSX structure and design complete

--- App.jsx
+++ App.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { languages } from "./languages";
 
-export default function Hangman() {
-  return <main>Game goes here</main>;
+export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = useState("react");
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const keyboardElements = alphabet
+    .split("")
+    .map((letter) => <button key={letter}>{letter}</button>);
+  console.log(keyboardElements);
+
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" key={lang.name} style={styles}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord
+    .split("")
+    .map((letter, index) => <span key={index}>{letter}</span>);
+
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You Win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="newGameButton">New Game</button>
+    </main>
+  );
 }
